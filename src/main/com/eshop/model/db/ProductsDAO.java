730b9cbc59c3d55868bd002c9badc308b844643e
Yaroslav <yaro01ir@gmail.com>
2021-05-14 01:08:20 +0300

    DAO Mock

---
+++ src/main/com/eshop/model/db/ProductsDAO.java
@@ -0,0 +1,50 @@
+package com.eshop.model.db;
+
+public class ProductsDAO {
+	public void insertProduct (Product product) {
+
+	}
+	public void insertCategory (Category category) {
+
+	}
+	public void updateProduct (Product product) {
+
+	}
+	public void updateCategory (Category category) {
+
+	}
+	public void deleteProduct (Product product) {
+
+	}
+	public void deleteCategory (Category category) {
+
+	}
+	public List <Product> getAllProducts () {
+
+		return null;
+	}
+	public List <Category> getAllCategories() {
+
+		return null;
+	}
+	public Lits <Product> getCategoryProducts (Category category) {
+
+		return null;
+	}
+	public Category getCategoryById (long id) {
+
+		return null;
+	}
+	public Category getCategoryByName (String name) {
+
+		return null;
+	}
+	public Product getCategoryById (long id) {
+
+		return null;
+	}
+	public Product getCategoryByPattern (Product pattern) {
+
+		return null;
+	}
+}
