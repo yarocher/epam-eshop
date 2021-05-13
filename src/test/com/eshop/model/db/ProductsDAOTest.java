edb3c286e1bf515f0edd5f52f3c7710f853af16c
Yaroslav <yaro01ir@gmail.com>
2021-05-14 01:18:34 +0300

    ProductsDAOTest mock

---
+++ src/test/com/eshop/model/db/ProductsDAOTest.java
@@ -0,0 +1,43 @@
+package com.eshop.model.db;
+
+public class ProductsDAOTest {
+	public void shouldInsertProduct () {
+
+	}
+	public void shouldInsertCategory () {
+
+	}
+	public void shouldUpdateProduct () {
+
+	}
+	public void shouldUpdateCategory () {
+
+	}
+	public void shouldDeleteProduct () {
+
+	}
+	public void shouldDeleteCategory () {
+
+	}
+	public void shouldGetAllProducts () {
+
+	}
+	public void shouldGetAllCategories() {
+
+	}
+	public void shouldGetCategoryProducts () {
+
+	}
+	public void shouldGetCategoryById () {
+
+	}
+	public void shouldGetCategoryByName () {
+
+	}
+	public void shouldGetProductById () {
+
+	}
+	public void shouldGetProductByPattern () {
+
+	}
+}
